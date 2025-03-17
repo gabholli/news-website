ded81c1b8ac13af10304e3a0745db0130c449925
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2025-03-17 15:37:55 -0700

    Added favicon to site

--- news-website/src/app/layout.tsx
+++ news-website/src/app/layout.tsx
@@ -13,6 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="shortcut icon" type="image/x-icon" href="/newspaper.ico" />
       <body
         className="font-serif bg-black text-white"
       >
