cd500779a3bdc9e57e2ad3ea8291c48d8060f9e0
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2025-03-17 14:22:42 -0700

    Started listing articles in main page

--- news-website/src/app/news/page.tsx
+++ news-website/src/app/news/page.tsx
@@ -3,16 +3,17 @@
 import axios from "axios"
 import React, { useEffect, useState } from "react"
 import Link from "next/link"
+import { NewsArticle } from "../types/types"
 
 export default function Articles() {
-    const [newsData, setNewsData] = useState([])
+    const [newsData, setNewsData] = useState<NewsArticle[]>([])
 
     useEffect(() => {
         axios
             .get("/api/newsAll")
             .then((response) => {
                 console.log('Client received:', response.data);
-                setNewsData(response.data)
+                setNewsData(response.data.articles)
             })
             .catch((error) => {
                 console.log("Error", error)
@@ -22,9 +23,28 @@ export default function Articles() {
             })
     }, [])
 
+    console.log(newsData)
+
+    const newsArticles = newsData?.map(article => {
+        return (
+            <div>
+                <h1>{article.title}</h1>
+            </div>
+        )
+    })
+
     return (
         <div>
-            News Here...
+            <main>
+                <div className="flex flex-col gap-y-8 p-6">
+                    <h1
+                        className="text-center font-bold text-2xl"
+                    >
+                        News Articles:
+                    </h1>
+                    {newsArticles}
+                </div>
+            </main>
         </div>
     )
 }
