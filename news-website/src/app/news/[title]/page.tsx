2d7db3d57745dddb09a5fe6379166898dbc03d3c
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2025-03-18 12:19:39 -0700

    Added top and bottom padding to details page

--- news-website/src/app/news/[title]/page.tsx
+++ news-website/src/app/news/[title]/page.tsx
@@ -33,7 +33,7 @@ export default function ArticleDetails() {
 
     const articleDetails = filteredArticles?.map(article => {
         return (
-            <div className="flex flex-col justify-center items-center w-3/4 md:w-2/3"
+            <div className="flex flex-col justify-center items-center w-3/4 md:w-2/3 py-8"
                 key={article.title}
             >
                 {article.urlToImage && (
