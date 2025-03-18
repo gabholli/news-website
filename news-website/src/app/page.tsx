052ca4305e442d60dbf284984fbf731e79111504
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2025-03-17 23:40:48 -0700

    Updated title page

--- news-website/src/app/page.tsx
+++ news-website/src/app/page.tsx
@@ -2,10 +2,13 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Test</h1>
+    <div className="flex flex-col justify-center items-center min-h-dvh
+    gap-y-4">
+      <h1 className="text-2xl">Welcome to Top Headlines</h1>
+      <p className="text-xl">We show the top headlines every day</p>
       <Link
         href="news"
+        className="underline text-lg"
       >
         To articles
       </Link>
