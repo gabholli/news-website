6368a0f32a7d83ab0ddbd41cebb6ed7d0cd982e9
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2025-03-18 22:10:29 -0700

    Changed styling of about page

--- news-website/src/app/about/page.tsx
+++ news-website/src/app/about/page.tsx
@@ -3,12 +3,12 @@ import Header from "../components/Header";
 export default function AboutPage() {
     return (
         <div
-            className="flex flex-col justify-center items-center min-h-dvh text-center gap-y-4"
+            className="flex flex-col justify-center items-center min-h-dvh text-center"
         >
             <div className="w-full">
                 <Header />
             </div>
-            <main className="flex-grow flex flex-col justify-center items-center">
+            <main className="flex-grow flex flex-col justify-center items-center px-4 gap-y-4">
                 <h1 className="text-2xl">About this site:</h1>
                 <p className="md:w-96 text-xl">
                     This site shows news stories built using the NewsAPI.
